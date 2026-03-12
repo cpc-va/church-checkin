5442f7a81b44061cd5702137bcf253be419342fa
cpc-va <cpcva.video@gmail.com>
2026-03-11 21:50:31 -0400

    Initial commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "church-checkin-v1";
+
+const urlsToCache = [
+  "./",
+  "./checkin.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+
+});
