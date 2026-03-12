efb5504a89487cfc165302d515fc707f9d2b6e68
Wah Yim <kin-wah.yim@associates.cisa.dhs.gov>
2026-03-11 23:47:08 -0400

    Test

--- sw.js
+++ sw.js
@@ -8,11 +8,29 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
 
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim(); // take control immediately
 });
 
 self.addEventListener("fetch", event => {
